a3ae78b426074eb4fa5bcc97b8b2059a8a8d5821
Bhagavan Reddy <bhagavan44@gmail.com>
2019-06-02 14:46:54 +0530

    Removed the unused import

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/Todo';
-import { url } from 'inspector';
 
 @Injectable({
   providedIn: 'root'
